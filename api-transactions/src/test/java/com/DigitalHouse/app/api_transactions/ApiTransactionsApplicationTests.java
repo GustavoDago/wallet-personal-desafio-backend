7467887668bf2712dc291c0293064cb046226ab4
GustavoDago <gustavodagoberto@gmail.com>
2025-02-26 00:45:45 -0300

    Agrego archivos faltantes de microservicio Cards, y varios más.

---
+++ api-transactions/src/test/java/com/DigitalHouse/app/api_transactions/ApiTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DigitalHouse.app.api_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
